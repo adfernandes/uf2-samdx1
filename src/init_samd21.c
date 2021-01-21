d8c298c232c73b5354922190b05bf2f036d2e331
turmary <turmary@126.com>
2021-01-21 15:27:47 +0000

    Remove: invalid GCLK_GEN2 setting if defined CRYSTALLESS

--- src/init_samd21.c
+++ src/init_samd21.c
@@ -28,17 +28,27 @@ void system_init(void) {
 
   NVMCTRL->CTRLB.bit.RWS = 1;
 
+  /*
+   * At reset:
+   * - OSC8M clock source is enabled with a divider by 8 (1MHz).
+   * - Generic Clock Generator 0 (GCLKMAIN) is using OSC8M as source.
+   */
+
 #if defined(CRYSTALLESS)
-  /* Configure OSC8M as source for GCLK_GEN 2 */
-  GCLK->GENDIV.reg = GCLK_GENDIV_ID(2);  // Read GENERATOR_ID - GCLK_GEN_2
-  gclk_sync();
-
-  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(2) | GCLK_GENCTRL_SRC_OSC8M_Val | GCLK_GENCTRL_GENEN;
-  gclk_sync();
-
   // Turn on DFLL with USB correction and sync to internal 8 mhz oscillator
+  /*
+   * SAM_D21_DA1_Family_DataSheet_DS40001882F
+   * USB Clock Recovery Mode
+   *   The SOF signal from USB device will be used as reference clock (CLK_DFLL_REF),
+   *   ignoring the selected generic clock reference.
+   *   When the USB device is connected, a SOF will be sent every 1ms,
+   *   thus DFLLVAL.MUX bits should be written to 0xBB80 (48000) to obtain a 48MHz clock.
+   */
+
+  // open-loop mode temperarily
   SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
   dfll_sync();
+
 
   SYSCTRL_DFLLVAL_Type dfllval_conf = {0};
   uint32_t coarse =( *((uint32_t *)(NVMCTRL_OTP4)
@@ -48,10 +58,10 @@ void system_init(void) {
   if (coarse == 0x3f) {
     coarse = 0x1f;
   }
-  dfllval_conf.bit.COARSE  = coarse;
+  dfllval_conf.bit.COARSE = coarse;
   // TODO(tannewt): Load this from a well known flash location so that it can be
   // calibrated during testing.
-  dfllval_conf.bit.FINE    = 0x1ff;
+  dfllval_conf.bit.FINE   = 0x1ff;
 
   SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_CSTEP( 0x1f / 4 ) | // Coarse step is 31, half of the max value
                          SYSCTRL_DFLLMUL_FSTEP( 10 ) |
@@ -66,15 +76,6 @@ void system_init(void) {
   dfll_sync();
   SYSCTRL->DFLLCTRL.reg |= SYSCTRL_DFLLCTRL_ENABLE ;
   dfll_sync();
-
-  GCLK_CLKCTRL_Type clkctrl={0};
-  uint16_t temp;
-  GCLK->CLKCTRL.bit.ID = 2; // GCLK_ID - DFLL48M Reference
-  temp = GCLK->CLKCTRL.reg;
-  clkctrl.bit.CLKEN = 1;
-  clkctrl.bit.WRTLOCK = 0;
-  clkctrl.bit.GEN = GCLK_CLKCTRL_GEN_GCLK0_Val;
-  GCLK->CLKCTRL.reg = (clkctrl.reg | temp);
 
 #else
 
